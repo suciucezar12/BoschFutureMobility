ce3f39230bdd25deaf7caf018f306ea0ab459ace
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-11-17 11:51:06 +0200

    picamera_demo4

--- picamera_demo.py
+++ picamera_demo.py
@@ -14,12 +14,23 @@ from picamera import PiCamera
 
 # Capture to a stream
 from io import BytesIO
-# create an in-memory stream
-my_stream = BytesIO()
+# # create an in-memory stream
+# my_stream = BytesIO()
+# camera = PiCamera()
+# camera.start_preview()
+# sleep(2)
+# camera.capture(my_stream, 'jpeg')
+
+
+
+
+# stream.2
+my_file = open('my_image.jpg', 'wb')
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
-camera.capture(my_stream, 'jpeg')
+camera.capture(my_file) # my_flie.flush() is called
+my_file.close()
 
 
 
