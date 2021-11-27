7db6390eb7c5c4432c026e3e7b1ca815f909eb6c
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-11-27 12:43:51 +0200

    CT4

--- CameraTest.py
+++ CameraTest.py
@@ -7,14 +7,13 @@ time.sleep(1)
 
 cap = cv2.VideoCapture(0)
 
-while True:
+# while True:
 
-    ret, frame = cap.read()
-    cv2.imshow("frame", frame)
-    cv2.waitKey(1)
-    if keyboard.is_pressed('q'):
-        print("Pressed 'q' => stopped streaming.")
-        break
+ret, frame = cap.read()
+cv2.imshow("frame", frame)
+cv2.waitKey(0)
+# if keyboard.is_pressed('q'):
+#     print("Pressed 'q' => stopped streaming.")
 
 cv2.destroyAllWindows()
 
