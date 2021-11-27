84cf6ecd85430e44cd8aaed8e39b458eba9e1c9e
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-11-27 12:25:38 +0200

    Testing the camera

---
+++ CameraTest.py
@@ -0,0 +1,20 @@
+import cv2
+import time
+
+import keyboard
+
+time.sleep(0.1)
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+while True:
+
+    ret, frame = cap.read()
+    cv2.imshow("frame", frame)
+    cv2.waitKey(1)
+    if keyboard.is_pressed('q'):
+        print("Pressed 'q' => stopped streaming.")
+        break
+
+cv2.destroyAllWindows()
+
