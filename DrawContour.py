abe76aa5adf18addbf624dc0dea095ead1918d7c
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-11-27 13:59:09 +0200

    Update DrawContour.py

--- DrawContour.py
+++ DrawContour.py
@@ -4,7 +4,7 @@ cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 cv2.imshow("frame", frame)
 cv2.waitKey(0)
-cv2.imread("ImageFromCamera.jpg", frame)
+cv2.imwrite("ImageFromCamera.jpg", frame)
 h = frame.shape[0]
 w = frame.shape[1]
 print("w = " + str(w) + " h = " + str(h))
