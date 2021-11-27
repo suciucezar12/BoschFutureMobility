8ea7c341cd7d6fd16616aa6cee61f2a301538f0e
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-11-27 14:16:00 +0200

    Update DrawContour.py

--- DrawContour.py
+++ DrawContour.py
@@ -4,8 +4,8 @@ cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 cv2.line(frame, (190, 260), (460, 260), (255, 0, 0))    # up base
 cv2.line(frame, (460, 260), (640, 480), (0, 255, 0))    # right
-# cv2.line(frame, (640, 480), (0, 480), (0, 0, 255))  # low base
-# cv2.line(frame, (0, 480), (240, 160), (255, 255, 255))  # left
+cv2.line(frame, (640, 480), (0, 480), (0, 0, 255))  # low base
+cv2.line(frame, (0, 480), (190, 260), (255, 255, 255))  # left
 cv2.imshow("frame", frame)
 cv2.waitKey(0)
 cv2.imwrite("ImageFromCamera.jpg", frame)
