9fb36c6cee0c257cd060de50857abca34aa6994a
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-01 17:01:21 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -20,7 +20,7 @@ while ret:
     cv2.imshow("Grayscale", frame_grayscale)
 
     # Apply Gaussian Blur
-    frame_blurred = cv2.GaussianBlur(frame_grayscale, (9, 9), cv2.BORDER_DEFAULT)
+    frame_blurred = cv2.GaussianBlur(frame_grayscale, (13, 13), cv2.BORDER_DEFAULT)
     cv2.imshow("Gaussian Blur", frame_blurred)
 
     # Apply Sobel on X axis
