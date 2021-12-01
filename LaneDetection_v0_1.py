f4e4a2d5ecd21bef58a3b68dc42602591c0d4bac
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-01 17:06:57 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -7,6 +7,7 @@ cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 
 while ret:
+    start = time.time()
     frame_copy = frame.copy()
     frame_copy = frame[int(frame.shape[0] / 2):, :]
     cv2.imshow("Frame", frame_copy)
@@ -27,6 +28,8 @@ while ret:
     # gX = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=ksize)
     frame_edge_detection = cv2.Canny(frame_blurred, 100, 200)
     cv2.imshow("Sobel", frame_edge_detection)
+    end = time.time()
+    print(end - start)
     cv2.waitKey(1)
 
 
