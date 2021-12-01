5790109d232198bdcf3b958ac7da4c4cb4fae02f
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-01 17:23:22 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -1,6 +1,9 @@
 import time
 import cv2
 
+def img():
+    pass
+
 time.sleep(0.1)
 cap = cv2.VideoCapture(0)
 
@@ -9,26 +12,27 @@ ret, frame = cap.read()
 while ret:
     start = time.time()
     frame_copy = frame.copy()
-    # take the bottom half of the frame
-    frame_copy = frame[int(frame.shape[0] / 2):, :]
-    cv2.imshow("Frame", frame_copy)
-
-    # rgb -> grayscale
-    frame_grayscale = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Grayscale", frame_grayscale)
-
-    # Apply Gaussian Blur
-    frame_blurred = cv2.GaussianBlur(frame_grayscale, (7, 7), cv2.BORDER_DEFAULT)
-    cv2.imshow("Gaussian Blur", frame_blurred)
-
-    # Apply Canny
-    frame_edge_detection = cv2.Canny(frame_blurred, 100, 200)
-    cv2.imshow("Canny", frame_edge_detection)
-
-    end = time.time()
-    print(end - start)
-
-    cv2.waitKey(1)
-    ret, frame = cap.read()
+    print(type(frame_copy))
+    # # take the bottom half of the frame
+    # frame_copy = frame[int(frame.shape[0] / 2):, :]
+    # cv2.imshow("Frame", frame_copy)
+    #
+    # # rgb -> grayscale
+    # frame_grayscale = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("Grayscale", frame_grayscale)
+    #
+    # # Apply Gaussian Blur
+    # frame_blurred = cv2.GaussianBlur(frame_grayscale, (7, 7), cv2.BORDER_DEFAULT)
+    # cv2.imshow("Gaussian Blur", frame_blurred)
+    #
+    # # Apply Canny
+    # frame_edge_detection = cv2.Canny(frame_blurred, 100, 200)
+    # cv2.imshow("Canny", frame_edge_detection)
+    #
+    # end = time.time()
+    # print(end - start)
+    #
+    # cv2.waitKey(1)
+    # ret, frame = cap.read()
 
 
