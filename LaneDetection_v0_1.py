6673165b70c3f979ef46e2298c63512ed09b9d53
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-01 17:13:49 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -9,28 +9,25 @@ ret, frame = cap.read()
 while ret:
     start = time.time()
     frame_copy = frame.copy()
+    # take the bottom half of the frame
     frame_copy = frame[int(frame.shape[0] / 2):, :]
     cv2.imshow("Frame", frame_copy)
-
-    # print("H = " + str(frame_copy.shape[0]) + ", W = " +  str(frame_copy.shape[1]))
-    # resize the image
-    #
 
     # rgb -> grayscale
     frame_grayscale = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Grayscale", frame_grayscale)
 
     # Apply Gaussian Blur
-    frame_blurred = cv2.GaussianBlur(frame_grayscale, (7, 7), cv2.BORDER_DEFAULT)
+    frame_blurred = cv2.GaussianBlur(frame_grayscale, (3, 3), cv2.BORDER_DEFAULT)
     cv2.imshow("Gaussian Blur", frame_blurred)
 
     # Apply Canny
-    # gX = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=ksize)
     frame_edge_detection = cv2.Canny(frame_blurred, 100, 200)
-    # frame_edge_detection = cv2.Sobel(frame_blurred, cv2.CV_32F, 1, 0, ksize=3)
     cv2.imshow("Canny", frame_edge_detection)
+
     end = time.time()
     print(end - start)
+
     cv2.waitKey(1)
     ret, frame = cap.read()
 
