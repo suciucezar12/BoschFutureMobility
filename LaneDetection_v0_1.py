1cc2503d9fbfe45dc11fffda3a7bd5becdf1c3d4
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-14 23:19:03 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -45,15 +45,19 @@ class LaneDetection:
             # fourth argument =  Threshold = Minimum length of line that should be detected = Nb of pixels which belong to that line
             # fifth argument = max allowed gap between line segments to treat them as single line
             lines = cv2.HoughLinesP(frame_edge_detection, rho=1, theta=np.pi / 180, threshold=100, minLineLength=10, maxLineGap=100)
-            frame_copy = frame_copy = frame[int(frame.shape[0] / 2):, :]
-            tolerance = 25
-            for line in lines:
-                x1, y1, x2, y2 = line[0]
-                cv2.line(frame_copy, (x1 - tolerance, y1), (x1 + tolerance, y1), (0, 255, 0), 3)
-                cv2.line(frame_copy, (x1, y1), (x2, y2), (255, 0, 0), 3)
-            cv2.imshow("Probabilistic Hough Transform", frame_copy)
-            cv2.waitKey(1)
 
+            print(lines)
+
+            # frame_copy = frame_copy = frame[int(frame.shape[0] / 2):, :]
+            # tolerance = 25
+            # for line in lines:
+            #     x1, y1, x2, y2 = line[0]
+            #     cv2.line(frame_copy, (x1 - tolerance, y1), (x1 + tolerance, y1), (0, 255, 0), 3)
+            #     cv2.line(frame_copy, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            #
+            # cv2.imshow("Probabilistic Hough Transform", frame_copy)
+            # cv2.waitKey(1)
+            cv2.waitKey(0)
             ret, frame = self.cap.read()
 
 
