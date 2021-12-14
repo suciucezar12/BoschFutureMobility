e0f9ab6e6ed4c15612865c77fdd5bdc8f8544f75
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-14 18:49:36 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -1,5 +1,6 @@
 import time
 import cv2
+import numpy as np
 
 
 class LaneDetection:
@@ -13,30 +14,37 @@ class LaneDetection:
         frame_copy = frame.copy()
 
         frame_copy = frame[int(frame.shape[0] / 2):, :]
-        cv2.imshow("Frame", frame_copy)
+        # cv2.imshow("Frame", frame_copy)
 
         # rgb -> grayscale
         frame_grayscale = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("Grayscale", frame_grayscale)
+        # cv2.imshow("Grayscale", frame_grayscale)
 
         # Apply Gaussian Blur
         frame_blurred = cv2.GaussianBlur(frame_grayscale, (7, 7), cv2.BORDER_DEFAULT)
-        cv2.imshow("Gaussian Blur", frame_blurred)
+        # cv2.imshow("Gaussian Blur", frame_blurred)
 
         # Apply Canny
         frame_edge_detection = cv2.Canny(frame_blurred, 100, 200)
-        cv2.imshow("Canny", frame_edge_detection)
+        # cv2.imshow("Canny", frame_edge_detection)
 
         end = time.time()
-        print(end - start)
+        # print(end - start)
 
-        cv2.waitKey(1)
+        # cv2.waitKey(1)
         return frame_edge_detection
 
     def run(self):
         ret, frame = self.cap.read()
         while ret:
-            self.apply_filter(ret, frame)
+            frame_edge_detection = self.apply_filter(ret, frame)
+            # lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap) second argument = P
+            # third argument = Theta accuracies ?
+            # fourth argument =  Threshold = Minimum length of line that should be detected = Nb of pixels which belong to that line
+            # fifth argument = max allowed gap between line segments to treat them as single line
+
+            # lines = cv2.HoughLinesP(frame_edge_detection, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+
             ret, frame = self.cap.read()
 
 
