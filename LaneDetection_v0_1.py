12162b71acd782a2126e3e33545624a1e9f7972d
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-14 22:48:34 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -46,10 +46,11 @@ class LaneDetection:
 
             lines = cv2.HoughLinesP(frame_edge_detection, 1, np.pi / 180, 100, minLineLength=10, maxLineGap=50)
             frame_copy = frame_copy = frame[int(frame.shape[0] / 2):, :]
+            tolerance = 50
             for line in lines:
                 x1, y1, x2, y2 = line[0]
+                cv2.line(frame_copy, (x2 - tolerance, y1), (x2 + tolerance, y1), (0, 255, 0), 3)
                 cv2.line(frame_copy, (x1, y1), (x2, y2), (255, 0, 0), 3)
-
             cv2.imshow("Probabilistic Hough Transform", frame_copy)
             cv2.waitKey(1)
 
