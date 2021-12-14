c08cf9a54a422673f39136cd7ce9fbe554039278
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-14 23:00:12 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -44,7 +44,7 @@ class LaneDetection:
             # fourth argument =  Threshold = Minimum length of line that should be detected = Nb of pixels which belong to that line
             # fifth argument = max allowed gap between line segments to treat them as single line
 
-            lines = cv2.HoughLinesP(frame_edge_detection, rho=1, theta=np.pi / 180, threshold=100, minLineLength=10, maxLineGap=100)
+            lines = cv2.HoughLinesP(lines, frame_edge_detection, rho=1, theta=np.pi / 180, threshold=100, minLineLength=10, maxLineGap=100)
             frame_copy = frame_copy = frame[int(frame.shape[0] / 2):, :]
             tolerance = 25
             if lines is not None:
