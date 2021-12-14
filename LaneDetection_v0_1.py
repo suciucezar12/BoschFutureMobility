1a68c5c447fcd185798bf0355d7cee0530069810
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-14 17:13:43 +0200

    Update LaneDetection_v0_1.py

--- LaneDetection_v0_1.py
+++ LaneDetection_v0_1.py
@@ -1,39 +1,44 @@
 import time
 import cv2
 
-def apply_filter(frame):
-    # take the bottom half of the frame
-    frame_copy = frame[int(frame.shape[0] / 2):, :]
-    cv2.imshow("Frame", frame_copy)
 
-    # rgb -> grayscale
-    frame_grayscale = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Grayscale", frame_grayscale)
+class LaneDetection:
 
-    # Apply Gaussian Blur
-    frame_blurred = cv2.GaussianBlur(frame_grayscale, (7, 7), cv2.BORDER_DEFAULT)
-    cv2.imshow("Gaussian Blur", frame_blurred)
+    def __init__(self):
+        time.sleep(0.1)
+        self.cap = cv2.VideoCapture(0)
 
-    # Apply Canny
-    frame_edge_detection = cv2.Canny(frame_blurred, 100, 200)
-    cv2.imshow("Canny", frame_edge_detection)
+    def apply_filter(self, ret, frame):
+        start = time.time()
+        frame_copy = frame.copy()
 
-    end = time.time()
-    print(end - start)
+        frame_copy = frame[int(frame.shape[0] / 2):, :]
+        cv2.imshow("Frame", frame_copy)
 
-    cv2.waitKey(1)
-    return frame_edge_detection
+        # rgb -> grayscale
+        frame_grayscale = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Grayscale", frame_grayscale)
+
+        # Apply Gaussian Blur
+        frame_blurred = cv2.GaussianBlur(frame_grayscale, (7, 7), cv2.BORDER_DEFAULT)
+        cv2.imshow("Gaussian Blur", frame_blurred)
+
+        # Apply Canny
+        frame_edge_detection = cv2.Canny(frame_blurred, 100, 200)
+        cv2.imshow("Canny", frame_edge_detection)
+
+        end = time.time()
+        print(end - start)
+
+        cv2.waitKey(1)
+        return frame_edge_detection
+
+    def run(self):
+        ret, frame = self.cap.read()
+        while ret:
+            self.apply_filter(ret, frame)
+            ret, frame = self.cap.read()
 
 
-time.sleep(0.1)
-cap = cv2.VideoCapture(0)
-
-ret, frame = cap.read()
-
-while ret:
-    start = time.time()
-    frame_copy = frame.copy()
-    frame_edge_detection = apply_filter(frame_copy)
-    ret, frame = cap.read()
-
-
+LD = LaneDetection()
+LD.run()
