5f8ed9ebcc15e5d3291d39dc406f74762bb5d594
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-16 21:17:40 +0200

    Update LaneDetection_StraightLines_NoObstacle.py

--- LaneDetection_StraightLines_NoObstacle.py
+++ LaneDetection_StraightLines_NoObstacle.py
@@ -92,8 +92,8 @@ class LaneDetection:
 
         x0 = float((Bl * Cr - Br * Cl) / (Cl * Ar - Cr * Al))
         y0 = float((Cl * Ar - Cr * Al) / (Al * Br - Ar * Bl))
-        print(x0 + " " + y0)
-        print(float((x0 - width / 2) / y0))
+        print(str(x0) + " " + str(y0))
+        print(str(float((x0 - width / 2) / y0)))
         theta = math.atan(float((x0 - width / 2) / y0))
         return theta
 
