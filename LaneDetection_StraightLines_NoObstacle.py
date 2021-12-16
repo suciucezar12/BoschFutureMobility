9121f0f33d74b88f21a2d5cc2a02ace3cfdedca9
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-16 18:58:54 +0200

    Update LaneDetection_StraightLines_NoObstacle.py

--- LaneDetection_StraightLines_NoObstacle.py
+++ LaneDetection_StraightLines_NoObstacle.py
@@ -43,7 +43,7 @@ class LaneDetection:
 
     def drawLane(self, image, lane, color):
         x1, y1, x2, y2 = lane
-        cv2.line(image, (x1, y1), (x2, y2), color, 3)
+        cv2.line(image, (x1, y1), (x2, y2), color, 5)
 
     def Run(self):
         ret, frame = self.cap.read()
@@ -54,7 +54,7 @@ class LaneDetection:
                                     maxLineGap=100)
             left_lanes = []
             right_lanes = []
-            frame_copy = frame[int(int(frame.shape[0] * 0.55)):, :]
+            frame_copy = frame[int(int(frame.shape[0] * 0.55)):, :]  # used for displaying
 
             if lines is not None:
                 # classify lanes based on their slope
@@ -66,27 +66,14 @@ class LaneDetection:
                     else:
                         right_lanes.append([x1, y1, x2, y2])
 
-                # # display candidates for left lane
-                # for lane in left_lanes:
-                #     x1, y1, x2, y2 = lane
-                #     cv2.line(frame_copy, (x1, y1), (x2, y2), (255, 0, 0), 3)
-                #
-                # # display candidates for right lane
-                # for lane in right_lanes:
-                #     x1, y1, x2, y2 = lane
-                #     cv2.line(frame_copy, (x1, y1), (x2, y2), (0, 0, 255), 3)
-
                 left_lane = self.Averagelanes(left_lanes)
                 right_lane = self.Averagelanes(right_lanes)
 
-                # x1, y1, x2, y2 = left_lane
-                # cv2.line(frame_copy, (x1, y1), (x2, y2), (255, 0, 0), 3)
                 self.drawLane(frame_copy, left_lane, (255, 0, 0))
-                # x1, y1, x2, y2 = right_lane
-                # cv2.line(frame_copy, (x1, y1), (x2, y2), (0, 0, 255), 3)
                 self.drawLane(frame_copy, right_lane, (0, 0, 255))
+
             cv2.imshow("Frame", frame)
-            cv2.imshow("PHT Candidates", frame_copy)
+            cv2.imshow("PHT", frame_copy)
             cv2.waitKey(1)
             ret, frame = self.cap.read()
 
