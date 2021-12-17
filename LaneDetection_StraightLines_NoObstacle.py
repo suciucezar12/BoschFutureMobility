8415312b34b7c96a1fbc0f66e5cda8a24fb17fc4
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-17 16:03:50 +0200

    Update LaneDetection_StraightLines_NoObstacle.py

--- LaneDetection_StraightLines_NoObstacle.py
+++ LaneDetection_StraightLines_NoObstacle.py
@@ -69,16 +69,16 @@ class LaneDetection:
         # print("width = " + str(width))
         x1l, y1l, x2l, y2l = left_lane
         y1l = -(y1l - height)
-        # print("y1l = " + str(y1l) + ", x1l = " + str(x1l))
+        print("y1l = " + str(y1l) + ", x1l = " + str(x1l))
         y2l = -(y2l - height)
-        # print("y2l = " + str(y2l) + ", x2l = " + str(x2l))
+        print("y2l = " + str(y2l) + ", x2l = " + str(x2l))
 
         x1r, y1r, x2r, y2r = right_lane
         # print("y2r before = " + str(y2r))
         y1r = -(y1r - height)
-        # print("y1r = " + str(y1r) + ", x1r = " + str(x1r))
+        print("y1r = " + str(y1r) + ", x1r = " + str(x1r))
         y2r = -(y2r - height)
-        # print("y2r = " + str(y2r) + ", x2r = " + str(x2r))
+        print("y2r = " + str(y2r) + ", x2r = " + str(x2r))
 
         L1 = self.General_Equation_Form(x1l, y1l, x2l, y2l)  # L1 -> left lane
         L2 = self.General_Equation_Form(x1r, y1r, x2r, y2r)  # l2 -> right lane
