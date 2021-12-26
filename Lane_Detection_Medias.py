62a5c830a9852bdd556bad275567b948dd56f51b
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-26 16:18:07 +0200

    Update Lane_Detection_Medias.py

--- Lane_Detection_Medias.py
+++ Lane_Detection_Medias.py
@@ -8,13 +8,13 @@ class LaneDetection:
 
     def __init__(self):
         time.sleep(0.2)  # let camera warm-up
-        self.cap = cv2.VideoCapture(1)
+        self.cap = cv2.VideoCapture(0)
 
     def run(self):
         ret, frame = self.cap.read()
         while ret:
             cv2.imshow("frame", frame)
-            cv2.waitKey(0)
+            cv2.waitKey(1)
             ret, frame = self.cap.read()
 
 
