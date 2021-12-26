6818e4c03bbb1532141b13cb5445e3962e0c3789
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2021-12-26 16:14:51 +0200

    Create Lane_Detection_Medias.py

---
+++ Lane_Detection_Medias.py
@@ -0,0 +1,22 @@
+import time
+
+import cv2
+import numpy as np
+
+
+class LaneDetection:
+
+    def __init__(self):
+        time.sleep(0.2)  # let camera warm-up
+        self.cap = cv2.VideoCapture(0)
+
+    def run(self):
+        ret, frame = self.cap.read()
+        while ret:
+            cv2.imshow("frame", frame)
+            cv2.waitKey(0)
+            ret, frame = self.cap.read()
+
+
+LD = LaneDetection()
+LD.run()
