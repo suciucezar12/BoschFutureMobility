6b6a6c75d303ece59972d558edab10792bb96788
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-01-07 15:46:20 +0200

    Update demo_camera.py

--- demo_camera.py
+++ demo_camera.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 
-time.sleep(2)
+time.sleep(0.2)
 
 cap = cv2.VideoCapture(0)
 
@@ -10,6 +10,6 @@ ret, frame = cap.read()
 while ret:
 	
 	cv2.imshow("", frame)
-	cv2.waitkey(1)
+	cv2.waitKey(1)
 	
 	re, frame = cap.read()
