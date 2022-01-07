2dfcd5899ab3a1f86b4ad05e6b2646910bdf3fd9
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-01-07 23:52:50 +0200

    Update Lane_Detection_Medias.py

--- Lane_Detection_Medias.py
+++ Lane_Detection_Medias.py
@@ -3,15 +3,31 @@ import time
 import cv2
 import numpy as np
 
-class Slide_Window():
-    pass
+
+class SlideWindow:
+    # dimensions
+    margin = None
+    height = None
+
+    # center point of down border
+    x0 = None
+    y0 = None
 
 
-class Lane_Detection:
+class LaneDetection:
 
     def __init__(self):
         time.sleep(0.2)  # let camera warm-up
         self.cap = cv2.VideoCapture(0)
+
+    # method used to obtain the starting points (x, 0) of left and right lane
+    def find_starting_points_lanes(self, frame):
+        partial_frame = frame[frame.shape[0] // 5:, :]
+        histogram = np.sum(partial_frame, axis=0)  # sum on each column (on each particular column) => histogram
+        size = len(histogram)
+        x_left = np.argmax(histogram[0: size // 2])  # choose x_left
+        x_right = np.argmax(histogram[size // 2:]) + size // 2  # choose x_right
+        return x_left, x_right
 
     def preprocessing_frame(self, frame):
         frame_copy = frame.copy()
@@ -31,8 +47,8 @@ class Lane_Detection:
         ret, frame = self.cap.read()
 
         while ret:
-
-            edge_frame = self.preprocessing_frame(frame)    # apply filters and selecting ROI
+            edge_frame = self.preprocessing_frame(frame)  # apply filters and selecting ROI
+            self.find_starting_points_lanes(frame)
 
             cv2.imshow("Preprocessing", edge_frame)
             cv2.imshow("Frame", frame)
@@ -40,5 +56,5 @@ class Lane_Detection:
             ret, frame = self.cap.read()
 
 
-LD = Lane_Detection()
+LD = LaneDetection()
 LD.run()
