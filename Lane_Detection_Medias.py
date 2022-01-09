e8ddf2d6e0b8222283c3ece8bb64c203f795d839
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-01-09 12:43:56 +0200

    Update Lane_Detection_Medias.py

--- Lane_Detection_Medias.py
+++ Lane_Detection_Medias.py
@@ -28,40 +28,41 @@ class LaneDetection:
 
     # method used to obtain the starting points (x) of left and right lane
     def get_starting_points_lanes(self, blurred_frame):
-        sobel_frame = cv2.Sobel(blurred_frame, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=5)
-        cv2.imshow("Sobel", sobel_frame)
-        cv2.waitKey(1)
-        partial_frame = sobel_frame[sobel_frame.shape[0] // 5:, :]
+
+        partial_frame = blurred_frame[blurred_frame.shape[0] // 5:, :]
+
+        # apply other filters to obtain better frame for histogram calculation
+
         histogram = np.sum(partial_frame, axis=0)  # sum on each column (on each particular column) => histogram
-        # print(histogram)
+
         size = len(histogram)
         x_left = np.argmax(histogram[0: size // 2])  # choose x_left
         x_right = np.argmax(histogram[size // 2:]) + size // 2  # choose x_right
 
+        print(f"histogram[{x_left}] = " + histogram[x_left])
         if histogram[x_left] == 0:  # no left lane
             x_left = None
 
+        print(f"histogram[{x_right}] = " + histogram[x_right])
         if histogram[x_right] == 0:  # no right lane
             x_right = None
 
         return x_left, x_right
 
-    # apply filters
+        # apply filters
+
     def preprocessing_frame(self, frame):
+
         frame_copy = frame.copy()
 
-        width = frame_copy.shape[1]
         height = frame_copy.shape[0]
-        frame_copy = frame_copy[int(0.5 * height):, :]  # crop image
+        frame_copy = frame_copy[int(0.5 * height):, :]  # crop image (select our ROI)
 
+        # Apply filters
         gray_frame = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
         blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), cv2.BORDER_DEFAULT)
-
-        self.get_starting_points_lanes(gray_frame)   # use here to no compute 2 times all preprocessing phases until edge detection phase
-
-        edge_frame = cv2.Canny(blurred_frame, 100, 200)
-
-        return edge_frame
+        # edge_frame = cv2.Canny(blurred_frame, 100, 200)
+        return blurred_frame
 
     # method for obtaining lanes
     def get_lanes(self, edge_frame):
@@ -71,11 +72,14 @@ class LaneDetection:
         ret, frame = self.cap.read()
 
         while ret:
-            edge_frame = self.preprocessing_frame(frame)  # apply filters and selecting ROI
+            # preprocessing frame
+            blurred_frame = self.preprocessing_frame(frame)
+            # find starting points of lanes (x, 0)
+            # x_left, x_right = self.get_starting_points_lanes(blurred_frame)
 
-            self.get_lanes(edge_frame)
+            edge_frame = cv2.Canny(blurred_frame, 100, 200)
 
-            cv2.imshow("Preprocessing", edge_frame)
+            cv2.imshow("Edge Detection", edge_frame)
             cv2.imshow("Frame", frame)
             cv2.waitKey(1)
             ret, frame = self.cap.read()
