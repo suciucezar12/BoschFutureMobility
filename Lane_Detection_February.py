3d372140903e4ca33722eafb832696198a931da3
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-14 22:05:56 +0200

    february

---
+++ Lane_Detection_February.py
@@ -0,0 +1,16 @@
+import time
+import cv2
+
+
+class LaneDetection:
+
+    def __init__(self):
+        time.sleep(0.1)
+        self.cap = cv2.VideoCapture(0)
+
+    def run(self):
+
+        frame = self.cap.read()
+
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(0)
