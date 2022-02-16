c1c9984c2a3b3fcad9eac789e6d1d3fcd806e975
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-16 18:35:46 +0200

    Update Lane_Detection_February.py

--- Lane_Detection_February.py
+++ Lane_Detection_February.py
@@ -8,22 +8,22 @@ class LaneDetection:
         time.sleep(0.1)
         self.cap = cv2.VideoCapture(0)
 
-    def draw_ROI(self, frame):
-        width_frame = 640
-        height_frame = 480
-
-        # select the 4 points of the trapezoid
-        pt1 = (0, 640)
-        pt2 = (50, 600)
-        pt3 = (590, 600)
-        pt4 = (480, 640)
-
-        cv2.line(frame, pt1, pt2, (255, 255, 0), 3)
-        cv2.line(frame, pt2, pt3, (255, 255, 0), 3)
-        cv2.line(frame, pt3, pt4, (255, 255, 0), 3)
-        cv2.line(frame, pt4, pt1, (255, 255, 0), 3)
-
-        return frame
+    # def draw_ROI(self, frame):
+    #     width_frame = 640
+    #     height_frame = 480
+    #
+    #     # select the 4 points of the trapezoid
+    #     pt1 = (0, 640)
+    #     pt2 = (50, 600)
+    #     pt3 = (590, 600)
+    #     pt4 = (480, 640)
+    #
+    #     cv2.line(frame, pt1, pt2, (255, 255, 0), 3)
+    #     cv2.line(frame, pt2, pt3, (255, 255, 0), 3)
+    #     cv2.line(frame, pt3, pt4, (255, 255, 0), 3)
+    #     cv2.line(frame, pt4, pt1, (255, 255, 0), 3)
+    #
+    #     return frame
 
     def run(self):
 
@@ -31,7 +31,7 @@ class LaneDetection:
 
         while True:
 
-            print(str(frame.shape[0]) + " " + str(frame.shape[1]))
+            # print(str(frame.shape[0]) + " " + str(frame.shape[1]))
             # Selecting ROI -> looking for a trapezoid where our lanes would always appear
             # base of the trapezoid is actually the bottom line of our frame
             # frame = self.draw_ROI(frame)
@@ -43,13 +43,13 @@ class LaneDetection:
 
             frame_copy = frame.copy()
 
-            cv2.line(frame_copy, (0, 640), (50, 600), (255, 255, 0), 3)
+            frame_copy = cv2.line(frame_copy, (0, 640), (50, 600), (255, 255, 0), 3)
             cv2.line(frame_copy, (50, 600), (590, 600), (255, 255, 0), 3)
             cv2.line(frame_copy, (590, 600), (480, 640), (255, 255, 0), 3)
             cv2.line(frame_copy, (480, 640), (0, 640), (255, 255, 0), 3)
 
             cv2.imshow("Frame", frame_copy)
-            cv2.waitKey(2)
+            cv2.waitKey(1)
 
             ret, frame = self.cap.read()
 
