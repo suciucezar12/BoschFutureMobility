62c395228f9e3847d91b98d0f7dfb59eabfeccd1
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-16 18:58:36 +0200

    Update Lane_Detection_February.py

--- Lane_Detection_February.py
+++ Lane_Detection_February.py
@@ -43,10 +43,10 @@ class LaneDetection:
 
             frame_copy = frame.copy()
 
-            cv2.line(frame_copy, (0, 640), (50, 600), (255, 255, 0), 3)
+            cv2.line(frame_copy, (0, 100), (50, 600), (255, 255, 0), 3)
             cv2.line(frame_copy, (50, 600), (590, 600), (255, 255, 0), 3)
             cv2.line(frame_copy, (590, 600), (480, 639), (255, 255, 0), 3)
-            cv2.line(frame_copy, (480, 639), (1, 639), (255, 255, 0), 3)
+            cv2.line(frame_copy, (480, 639), (0, 100), (255, 255, 0), 3)
 
             cv2.imshow("Frame", frame_copy)
             cv2.waitKey(1)
