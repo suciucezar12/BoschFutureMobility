1e5ae7f654b6fcfc89d6ce60b87b5134e50cd445
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-18 21:23:31 +0200

    Update calibrate.py

--- video2calibration/calibrate.py
+++ video2calibration/calibrate.py
@@ -3,11 +3,11 @@ import numpy as np
 import cv2
 import os
 import argparse
-import yaml
+# import yaml
 import pickle
 from glob import glob
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Calibrate camera using a video of a chessboard or a sequence of images.')
     parser.add_argument('input',nargs="?", help='input video file or glob mask')
     parser.add_argument('out',nargs="?",help='output calibration yaml file')
@@ -70,12 +70,12 @@ if __name__ == '__main__':
             cv2.drawChessboardCorners(img_chess, pattern_size, corners, found)
             cv2.imwrite(os.path.join(args.debug_dir, '%04d.png' % i), img_chess)
         if not found:
-            print 'not found'
+            print('not found')
             continue
         img_points.append(corners.reshape(1, -1, 2))
         obj_points.append(pattern_points.reshape(1, -1, 3))
 
-        print 'ok'
+        print('ok')
 
     if args.corners:
         with open(args.corners, 'wb') as fw:
@@ -86,9 +86,9 @@ if __name__ == '__main__':
 
     print('\nPerforming calibration...')
     rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, (w, h), None, None)
-    print "RMS:", rms
-    print "camera matrix:\n", camera_matrix
-    print "distortion coefficients: ", dist_coefs.ravel()
+    print("RMS:", rms)
+    print("camera matrix:\n", camera_matrix)
+    print("distortion coefficients: ", dist_coefs.ravel())
 
     # # fisheye calibration
     # rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.fisheye.calibrate(
