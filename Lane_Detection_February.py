a6af9501c306082eda2bb4a037ca7993d140b2da
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-19 15:17:30 +0200

    Update Lane_Detection_February.py

--- Lane_Detection_February.py
+++ Lane_Detection_February.py
@@ -10,8 +10,8 @@ class LaneDetection:
 
         ''' The coordinates of Region of Interest (ROI) '''
         self.x_top = 270
-        self.y_left_top = 120
-        self.y_right_top = 520
+        self.y_left_top = 100
+        self.y_right_top = 540
         # coords are [y, x]
         self.roi_coords = np.array([[0, 480], [self.y_left_top, self.x_top], [self.y_right_top, self.x_top], [640, 480]], dtype=np.int32)
         ''' =========================================== '''
