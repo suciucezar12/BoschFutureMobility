0399bd046f2734238b1b62eec0a982b5bdac125e
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-20 20:21:30 +0200

    Update Lane_Detection_February.py

--- Lane_Detection_February.py
+++ Lane_Detection_February.py
@@ -12,7 +12,7 @@ class LaneDetection:
         self.input_coordinates_IPM = np.array([[0, 480], [self.y_left_top, self.x_top], [self.y_right_top, self.x_top], [640, 480]], dtype=np.float32)
         self.output_coordinates_IPM = np.array([[199, 36], [417, 0], [439, 444], [205, 410]], dtype=np.float32) # Output coordinates calculated manually in our flat real word plane of the road
         self.matrix_IPM = cv2.getPerspectiveTransform(self.input_coordinates_IPM, self.output_coordinates_IPM)
-        ''' =========================================== '''
+        ''' ================================================================================================================================ '''
 
         time.sleep(1)
         self.cap = cv2.VideoCapture(0)
@@ -23,6 +23,9 @@ class LaneDetection:
         frame_IPM = cv2.warpPerspective(frame, self.matrix_IPM, (frame_IPM_width, frame_IPM_height), flags=cv2.INTER_LINEAR)
         rotation_matrix = cv2.getRotationMatrix2D((frame_IPM_width / 2, frame_IPM_height / 2), 90, 1.0)
         frame_IPM_rotated = cv2.warpAffine(frame_IPM, rotation_matrix, (frame_IPM_width, frame_IPM_height))
+        margin_x_crop = 30
+        frame_IPM_final = frame_IPM_rotated[margin_x_crop, frame_IPM_rotated.shape[0] - margin_x_crop:]
+
         return frame_IPM_rotated
 
     def run(self):
@@ -34,6 +37,7 @@ class LaneDetection:
             start = time.time()  # measure (time_computing / frame)
 
             cv2.polylines(frame, np.int32([self.input_coordinates_IPM]), True, (0, 255, 255))
+
             frame_IPM = self.get_IPM_frame(frame)
 
             cv2.imshow("IPM", frame_IPM)
