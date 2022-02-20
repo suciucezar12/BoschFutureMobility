7ec468dfc7c8adf1807133d19f10cf12b31f370c
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-20 16:28:46 +0200

    Update Lane_Detection_February.py

--- Lane_Detection_February.py
+++ Lane_Detection_February.py
@@ -71,11 +71,11 @@ class LaneDetection:
 
             out = cv2.warpPerspective(frame, M, (500, 500), flags=cv2.INTER_LINEAR)
 
-            M = cv2.getRotationMatrix2D((320, 240), -90, 1.0)
-            rotated = cv2.warpAffine(frame, M, (640, 480))
+            # M = cv2.getRotationMatrix2D((320, 240), -90, 1.0)
+            # rotated = cv2.warpAffine(frame, M, (640, 480))
 
             cv2.imshow("IPM", out)
-            cv2.imshow("Frame", rotated)
+            cv2.imshow("Frame", frame)
             cv2.waitKey(1)
 
             end = time.time()
