833af5118303164cb04052eb4adc1d1ec832e024
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-23 12:25:42 +0200

    Update Lane_Detection_MidGate.py

--- Lane_Detection_MidGate.py
+++ Lane_Detection_MidGate.py
@@ -25,7 +25,7 @@ class LaneDetection:
         frame_ROI_blurred = cv2.GaussianBlur(frame_ROI_gray, (11, 11), 0)
         # cv2.imshow("ROI_Blurred", frame_ROI_blurred)
 
-        frame_ROI_preprocessed = cv2.Canny(frame_ROI_blurred, 100, 200)
+        frame_ROI_preprocessed = cv2.Canny(frame_ROI_blurred, 150, 255)
 
         return frame_ROI_preprocessed
 
