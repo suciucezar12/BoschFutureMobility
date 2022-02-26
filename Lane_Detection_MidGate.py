acf23cfb1a1eb6665108aaf1caf8042585ed1f54
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-26 22:39:18 +0200

    Update Lane_Detection_MidGate.py

--- Lane_Detection_MidGate.py
+++ Lane_Detection_MidGate.py
@@ -252,6 +252,7 @@ class LaneDetection:
             theta = self.get_theta(frame_ROI_preprocessed, frame_ROI)
             if theta != -1000:  # we didn't detect any line
                 theta_average = 0.4 * theta_average + 0.6 * theta
+            print(theta_average)
 
 
             cv2.imshow("ROI", frame_ROI)
