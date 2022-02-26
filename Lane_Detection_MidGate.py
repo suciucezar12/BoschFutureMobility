ab49bfbf77d2eb4f41d5095768ea268f3a71973c
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-02-26 20:41:10 +0200

    Update Lane_Detection_MidGate.py

--- Lane_Detection_MidGate.py
+++ Lane_Detection_MidGate.py
@@ -167,7 +167,7 @@ class LaneDetection:
 
         y_cv_vanishing_point = int((y_cv_right_line + y_cv_left_line) / 2)
         print(y_cv_vanishing_point - y_cv_right_line)
-        cv2.line(frame_ROI, (int(frame_ROI.shape[1] / 2), frame_ROI.shape[0]), (y_cv_vanishing_point, x_cv_theta), (232, 32, 1), 5)
+        cv2.line(frame_ROI, (int(frame_ROI.shape[1] / 2) - 50, frame_ROI.shape[0]), (y_cv_vanishing_point, x_cv_theta), (232, 32, 1), 5)
 
         return y_cv_vanishing_point, x_cv_theta
 
@@ -189,7 +189,7 @@ class LaneDetection:
             y_cv_vanishing_point -= offset_center_road
 
         cv2.line(frame_ROI, (y_cv_vanishing_point, x_cv_theta), (y_cv_line, x_cv_theta), (200, 200, 200), 2)
-        cv2.line(frame_ROI, (int(frame_ROI.shape[1] / 2), frame_ROI.shape[0]), (y_cv_vanishing_point, x_cv_theta),
+        cv2.line(frame_ROI, (int(frame_ROI.shape[1] / 2) - 50, frame_ROI.shape[0]), (y_cv_vanishing_point, x_cv_theta),
                  (232, 32, 1), 5)
 
         return y_cv_vanishing_point, x_cv_theta
