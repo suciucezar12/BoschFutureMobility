a05aa46047860e14b599352bd1b4f57d2a2cc180
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-02 13:28:09 +0200

    Update Lane_Detection_MidGate.py

--- Lane_Detection_MidGate.py
+++ Lane_Detection_MidGate.py
@@ -301,8 +301,8 @@ class LaneDetection:
 
             # cv2.imshow("IPM", frame_IPM)
             # cv2.imshow("Frame_ROI Preprocessed", frame_ROI_preprocessed)
-            cv2.imshow("ROI", frame_ROI)
-            # cv2.imshow("Frame", frame)
+            # cv2.imshow("ROI", frame_ROI)
+            cv2.imshow("Frame", frame)
             cv2.waitKey(1)
 
             end = time.time()
