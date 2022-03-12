c6f84cd84edcfdbb1738bb7c2bef45e42428d313
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-12 21:26:56 +0200

    Update IPM_Midgate.py

--- IPM_Midgate.py
+++ IPM_Midgate.py
@@ -11,21 +11,21 @@ width_ROI_IPM = 547
 
 ret, frame = cap.read()
 
+# clockwise selection of corners
+src_points = np.array([[0, 0], [width_ROI_IPM, 0], [width_ROI_IPM, height_ROI_IPM], [0, height_ROI_IPM]], dtype=np.float32)
+# testing
+# dst_points = np.array([[0, 0], [63.7, 0], [52.7, 25.6], [11, 25.6]])
+# initial
+dst_points = np.array([[0, 3.1], [66.7, 0], [53.5, 24.6], [11, 25.6]])
+dst_points = np.array([[int(y_cv / pixel_resolution), int(x_cv / pixel_resolution)] for [y_cv, x_cv] in dst_points], dtype=np.float32)
+print(dst_points)
+H = cv2.getPerspectiveTransform(src_points, dst_points)
+
 while True:
 
     cv2.line(frame, (0, x_cv_ROI + 5), (640, x_cv_ROI + 5), (0, 0, 255), 1)
 
     frame_ROI = frame[x_cv_ROI:, :]
-
-    # clockwise selection of corners
-    src_points = np.array([[0, 0], [width_ROI_IPM, 0], [width_ROI_IPM, height_ROI_IPM], [0, height_ROI_IPM]], dtype=np.float32)
-    # testing
-    # dst_points = np.array([[0, 0], [63.7, 0], [52.7, 25.6], [11, 25.6]])
-    # initial
-    dst_points = np.array([[0, 3.1], [66.7, 0], [53.5, 24.6], [11, 25.6]])
-    dst_points = np.array([[int(y_cv / pixel_resolution), int(x_cv / pixel_resolution)] for [y_cv, x_cv] in dst_points], dtype=np.float32)
-
-    H = cv2.getPerspectiveTransform(src_points, dst_points)
     frame_ROI_IPM = cv2.warpPerspective(frame_ROI, H, (width_ROI_IPM, height_ROI_IPM), flags=cv2.INTER_LINEAR)
 
     cv2.imshow("IPM", frame_ROI_IPM)
