a97623bb01551fff98e7f252747e67de7433e647
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-12 18:24:55 +0200

    Update IPM_Midgate.py

--- IPM_Midgate.py
+++ IPM_Midgate.py
@@ -1,7 +1,13 @@
 import cv2
+import numpy as np
 
 x_cv_ROI = 270
 cap = cv2.VideoCapture(0)
+
+pixel_resolution = 0.122 # how many centimeters per pixel
+height_ROI_IPM = 210    # calculated related to pixel_resolution and the real dimensions
+width_ROI_IPM = 547
+
 
 ret, frame = cap.read()
 
@@ -11,6 +17,15 @@ while True:
 
     frame_ROI = frame[x_cv_ROI:, :]
 
+    # clockwise selection of corners
+    src_points = np.array([[0, 0], [width_ROI_IPM, 0], [width_ROI_IPM, height_ROI_IPM], [0, height_ROI_IPM]])
+    dst_points = np.array([[0, 3.1], [66.7, 0], [53.5, 24.6], [11, 25.6]])
+    dst_points = [[int(y_cv / pixel_resolution), int(x_cv / pixel_resolution)] for [y_cv, x_cv] in dst_points]
+
+    H = cv2.getPerspectiveTransform(src_points, dst_points)
+    frame_ROI_IPM = cv2.warpPerspective(frame_ROI, H, (width_ROI_IPM, height_ROI_IPM), flags=cv2.INTER_LINEAR)
+
+    cv2.imshow("IPM", frame_ROI_IPM)
     cv2.imshow("ROI", frame_ROI)
     cv2.imshow("Frame", frame)
     cv2.waitKey(1)
