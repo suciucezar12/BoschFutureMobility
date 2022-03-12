21bd2baf20ce6bc86c7d6a0be860e962c9de2171
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-12 17:31:44 +0200

    Update IPM_Midgate.py

--- IPM_Midgate.py
+++ IPM_Midgate.py
@@ -1,10 +1,14 @@
 import cv2
 
+x_cv_ROI = 210
 cap = cv2.VideoCapture(0)
 
 ret, frame = cap.read()
 
 while True:
+
+    cv2.line(frame, (0, x_cv_ROI), (640, x_cv_ROI), (0, 0, 255), 1)
+
     cv2.imshow("Frame", frame)
     cv2.waitKey(1)
     ret, frame = cap.read()
