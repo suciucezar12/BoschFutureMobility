0f92e55f24e9eedb14dbc6b04ba7954197f49afe
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-12 18:12:08 +0200

    Update IPM_Midgate.py

--- IPM_Midgate.py
+++ IPM_Midgate.py
@@ -7,8 +7,11 @@ ret, frame = cap.read()
 
 while True:
 
-    cv2.line(frame, (0, x_cv_ROI), (640, x_cv_ROI), (0, 0, 255), 1)
+    cv2.line(frame, (0, x_cv_ROI + 5), (640, x_cv_ROI), (0, 0, 255), 1)
 
+    frame_ROI = frame[:, x_cv_ROI:]
+
+    cv2.imshow("ROI", frame_ROI)
     cv2.imshow("Frame", frame)
     cv2.waitKey(1)
     ret, frame = cap.read()
