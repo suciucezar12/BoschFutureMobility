084723c363e6ff7418876e096544f9fd31532a87
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-13 18:05:33 +0200

    Update LaneDetectionMidgate.py

--- LaneDetectionMidgate.py
+++ LaneDetectionMidgate.py
@@ -177,10 +177,10 @@ class LaneDetection:
         # take the top points of our lines
         # middle of the line determined by these 2 points will be our vanishing point
         y_cv_IPM_vp = int((left_line_IPM[0][2] + right_line_IPM[0][2]) / 2)
-        x_cv_IPM_vp = int((left_line_IPM[0][1] + right_line_IPM[0][3]) / 2)
+        x_cv_IPM_vp = int((left_line_IPM[0][3] + right_line_IPM[0][3]) / 2)
         # print(y_cv_IPM_vp)
         # print(x_cv_IPM_vp)
-        cv2.circle(frame_ROI_IPM, (y_cv_IPM_vp, x_cv_IPM_vp), 10, (0, 0, 0))
+        cv2.circle(frame_ROI_IPM, (y_cv_IPM_vp, x_cv_IPM_vp), 10, (255, 255, 255))
 
 
     def get_theta(self, frame_ROI, frame_ROI_IPM=None):  # get the steering angle
