cf3056951213f8a70f931234231b86df7b871234
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-13 16:16:49 +0200

    Update IPM_Midgate.py

--- IPM_Midgate.py
+++ IPM_Midgate.py
@@ -30,11 +30,11 @@ while True:
     frame_ROI_IPM = cv2.warpPerspective(frame_ROI, H, (width_ROI_IPM, height_ROI_IPM), flags=cv2.INTER_LINEAR)  # CV2.INTER_LINEAR
 
     # test transformation on a set of points
-    set_points = np.array([[[20, 30]]], dtype=np.float32)
-    print(set_points[0][0])
-    cv2.circle(frame_ROI, tuple(set_points[0][0]), 5, (0, 255, 0), 1)
+    set_points = np.array([[20, 30]], dtype=np.float32)
+    # print(set_points[0][0])
+    cv2.circle(frame_ROI, tuple(set_points[0]), 5, (0, 255, 0), 1)
     # transform set points in new plane
-    IPM_set_points = cv2.perspectiveTransform(set_points, H)[0]
+    IPM_set_points = cv2.perspectiveTransform(set_points, H)
     print(IPM_set_points)
     cv2.circle(frame_ROI_IPM, tuple(IPM_set_points[0]), 5, (255, 0, 0), 1)
 
@@ -43,6 +43,6 @@ while True:
     cv2.imshow("Frame", frame)
     cv2.waitKey(1)
 
-    print(time.time() - start)
+    # print(time.time() - start)
 
     ret, frame = cap.read()
