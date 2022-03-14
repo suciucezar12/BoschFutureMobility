83d212888735713298509ceb3e4509938b6ab425
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-14 22:44:57 +0200

    Update LaneDetectionMidgate.py

--- LaneDetectionMidgate.py
+++ LaneDetectionMidgate.py
@@ -257,7 +257,7 @@ class LaneDetection:
 
             theta = self.get_theta(frame_ROI, frame_ROI_IPM=None)
 
-            cv2.putText(img=frame_ROI, text=str(theta), org=(self.height_ROI - 100, int(self.width_ROI / 2) + 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1,
+            cv2.putText(img=frame_ROI, text=str(theta), org=(150, 150), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1,
                         color=(0, 255, 0), thickness=3)
 
             print("time: {}".format(time.time() - start))
