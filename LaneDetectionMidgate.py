ddca2b42e6cf62b226ed83100c3925dd59bde89b
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-03-15 17:45:16 +0200

    Update LaneDetectionMidgate.py

--- LaneDetectionMidgate.py
+++ LaneDetectionMidgate.py
@@ -253,9 +253,9 @@ class LaneDetection:
         while True:
             start = time.time()
             frame_ROI = frame[self.x_cv_ROI:, :]
-            # frame_ROI_IPM = cv2.warpPerspective(frame_ROI, self.H, (self.width_ROI_IPM, self.height_ROI_IPM), flags=cv2.INTER_NEAREST)
+            frame_ROI_IPM = cv2.warpPerspective(frame_ROI, self.H, (self.width_ROI_IPM, self.height_ROI_IPM), flags=cv2.INTER_NEAREST)
 
-            theta = self.get_theta(frame_ROI, frame_ROI_IPM=None)
+            theta = self.get_theta(frame_ROI, frame_ROI_IPM= frame_ROI_IPM)
 
             cv2.putText(img=frame_ROI, text=str(theta), org=(350, 200), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1,
                         color=(0, 255, 0), thickness=3)
@@ -264,7 +264,7 @@ class LaneDetection:
 
             # cv2.imshow("Frame", frame)
             cv2.imshow("ROI", frame_ROI)
-            # cv2.imshow("IPM", frame_ROI_IPM)
+            cv2.imshow("IPM", frame_ROI_IPM)
             cv2.waitKey(1)
 
             ret, frame = self.cap.read()
