3574934c12450f1d0d16fb3f739370e15ee23190
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-06 22:20:19 +0300

    Update utils.py

--- BoschFutureMobility/utils.py
+++ BoschFutureMobility/utils.py
@@ -146,7 +146,7 @@ class Utils:
     def get_line_IPM(self, line):
         y1_cv, x1_cv, y2_cv, x2_cv = line
         src_points = np.array([[[y1_cv, x1_cv], [y2_cv, x2_cv]]], dtype=np.float32)
-        dest_points = cv2.perspectiveTransform(src_points, self.H)[0]
+        dest_points = cv2.perspectiveTransform(src_points, self.H)[0][0]
         print(dest_points)
 
     def get_intercept_theta(self, line):
