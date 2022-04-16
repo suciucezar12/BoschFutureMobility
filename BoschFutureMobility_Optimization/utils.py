8ec1eb9ad80f065f23db3a6a262ecda0a1db2419
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-16 15:04:16 +0300

    Update utils.py

--- BoschFutureMobility_Optimization/utils.py
+++ BoschFutureMobility_Optimization/utils.py
@@ -37,8 +37,8 @@ class Utils:
                 y_points.append(x2_cv)
 
                 # create more data points on the line for better precision in our line estimation
-                num = 1
-                for y_cv in np.linspace(y1_cv, y2_cv, num)[1:]:
+                num = 3
+                for y_cv in np.linspace(y1_cv, y2_cv, num):
                     x_points.append(y_cv)
                     x_cv = line.coeff[1] * y_cv + line.coeff[0]
                     y_points.append(x_cv)
