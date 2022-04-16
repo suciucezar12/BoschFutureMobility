fc645c1b2b127302ebe74cfc64d21bc73f8153c0
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-16 15:46:32 +0300

    Update lane_detection.py

--- BoschFutureMobility_Optimization/lane_detection.py
+++ BoschFutureMobility_Optimization/lane_detection.py
@@ -185,9 +185,9 @@ class LaneDetection:
 
             # select our ROI
             frame_ROI = frame[self.x_cv_ROI:, :]
-            # frame_ROI_IPM = cv2.warpPerspective(frame_ROI, self.H, (self.width_ROI_IPM, self.height_ROI_IPM),
-            #                                     flags=cv2.INTER_NEAREST)
-            frame_ROI_IPM = None
+            frame_ROI_IPM = cv2.warpPerspective(frame_ROI, self.H, (self.width_ROI_IPM, self.height_ROI_IPM),
+                                                flags=cv2.INTER_NEAREST)
+            # frame_ROI_IPM = None
             offset, theta, intersection = self.lane_detection(frame_ROI, frame_ROI_IPM=frame_ROI_IPM)
 
             if offset is not None:
