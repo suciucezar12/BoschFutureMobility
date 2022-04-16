aa3fa1a9c7d4e00b7bb7a52b7a5d136d697a9a33
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-16 10:43:24 +0300

    Update test.py

--- BoschFutureMobility_Optimization/test.py
+++ BoschFutureMobility_Optimization/test.py
@@ -7,7 +7,11 @@ ret, frame = cap.read()
 
 while True:
 
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_blurred = cv2.GaussianBlur(frame_gray, (5, 5), 0)
+    frame_canny = cv2.Canny(frame_blurred, 180, 255)
+    cv2.imshow("Canny", frame_canny)
     cv2.imshow("Frame", frame)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
 
     _, frame = cap.read()
