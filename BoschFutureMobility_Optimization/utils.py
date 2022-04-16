a2dd50918d81d60116a53546bd8a12d46baca19d
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-16 14:42:11 +0300

    Update utils.py

--- BoschFutureMobility_Optimization/utils.py
+++ BoschFutureMobility_Optimization/utils.py
@@ -42,7 +42,7 @@ class Utils:
                     x_points.append(y_cv)
                     x_cv = line.coeff[1] * y_cv + line.coeff[0]
                     y_points.append(x_cv)
-                    cv2.circle(frame_ROI, (y_cv, int(x_cv)), 3, (0, 0, 255), 1)
+                    cv2.circle(frame_ROI, (int(y_cv), int(x_cv)), 3, (0, 0, 255), 1)
                 # -------------------------------------------------------------------------------
 
             # estimate our lane
