91443c01550a553adfe2d1065304b6b4ba770c93
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-18 16:30:13 +0300

    Update test_filters.py

--- BoschFutureMobility/test_filters.py
+++ BoschFutureMobility/test_filters.py
@@ -11,13 +11,17 @@ while True:
 
     bgr_gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
 
-    h, s, v = cv2.split(hsv)
+    hls = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
+
+    h, l, s = cv2.split(hls)
+
+    # h, s, v = cv2.split(hsv)
 
     cv2.imshow("Frame", frame)
     # cv2.imshow("HSV", hsv)
     cv2.imshow("H", h)
+    cv2.imshow("L", l)
     cv2.imshow("S", s)
-    cv2.imshow("V", v)
     cv2.waitKey(1)
 
     _, frame = cap.read()
