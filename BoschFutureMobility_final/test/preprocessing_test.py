73213b8c9bf883dfda1ef8d206f4147ec3e1047d
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-20 15:57:50 +0300

    Update preprocessing_test.py

--- BoschFutureMobility_final/test/preprocessing_test.py
+++ BoschFutureMobility_final/test/preprocessing_test.py
@@ -77,10 +77,10 @@ while True:
     cv2.imshow("Bilateral", bilateral)
 
     # cv2.imshow("Gamma correction", gamma_image)
-    # cv2.imshow("Alpha_Beta image", alpha_beta_image)
+    cv2.imshow("Alpha_Beta image", alpha_beta_image)
     # cv2.imshow("Alpha_Beta_Gamma image", alpha_beta_gamma_image)
 
-    # cv2.imshow("Histogram equalization", hist_eq)
+    cv2.imshow("Histogram equalization", hist_eq)
 
     cv2.waitKey(1)
 
