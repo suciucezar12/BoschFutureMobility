a36177f3eefe0302514a1c943fb577c583ac2a2e
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-20 14:56:29 +0300

    Update preprocessing_test.py

--- BoschFutureMobility_final/test/preprocessing_test.py
+++ BoschFutureMobility_final/test/preprocessing_test.py
@@ -34,12 +34,12 @@ while True:
     alpha_beta_image = cv2.convertScaleAbs(grayscale, alpha=alpha, beta=beta)
 
     # gamma correction
-    gamma = 1.8
+    gamma = 1.2
     table = np.array([((i / 255) ** gamma) * 255 for i in range(256)], np.uint8)
     gamma_image = cv2.LUT(grayscale, table)
 
     # alpha_beta + gamma
-    alpha = 1.8
+    alpha = 1.2
     beta = 0
     alpha_beta_image1 = cv2.convertScaleAbs(grayscale, alpha=alpha, beta=beta)
     gamma = 1.8
