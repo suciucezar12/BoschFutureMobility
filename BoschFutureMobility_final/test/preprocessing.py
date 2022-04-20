5eff0fccbbd0283d075894129cfa5f051bd2f308
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-20 16:16:14 +0300

    asdsa

---
+++ BoschFutureMobility_final/test/preprocessing.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+
+def preprocessing(frame, alpha, beta, gamma):
+    grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    contrast_frame = cv2.convertScaleAbs(grayscale_frame, alpha=alpha, beta=beta)
+    bilateral_frame = cv2.bilateralFilter(contrast_frame, 9, 10, 10)
+    return bilateral_frame
+
+while True:
+
+    # ROI
+    x_cv_ROI = 320
+    frame_ROI = frame[x_cv_ROI:, :]
+    
+    preprocessed_frame_ROI = preprocessing(frame_ROI, alpha=1.5, beta=0, gamma=0)
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Final Frame", preprocessed_frame_ROI)
+    cv2.waitKey(1)
+    _, frame = cap.read()
