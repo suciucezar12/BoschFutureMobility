b6bd75eef6e874ec0fed3cb267e4d09a6208bc6a
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-20 14:50:07 +0300

    Update preprocessing_test.py

--- BoschFutureMobility_final/test/preprocessing_test.py
+++ BoschFutureMobility_final/test/preprocessing_test.py
@@ -29,7 +29,7 @@ while True:
     bilateral = cv2.bilateralFilter(grayscale, 9, 10, 15)
 
     # increase contrast
-    alpha = 1.2
+    alpha = 1.8
     beta = 0
     alpha_beta_image = cv2.convertScaleAbs(grayscale, alpha=alpha, beta=beta)
 
@@ -39,7 +39,7 @@ while True:
     gamma_image = cv2.LUT(grayscale, table)
 
     # alpha_beta + gamma
-    alpha = 1.2
+    alpha = 1.8
     beta = 0
     alpha_beta_image1 = cv2.convertScaleAbs(grayscale, alpha=alpha, beta=beta)
     gamma = 1.5
