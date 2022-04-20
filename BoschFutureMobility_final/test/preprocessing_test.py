b70d76293e093b64d32e47480f400979b2097c1c
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-04-20 15:58:17 +0300

    Update preprocessing_test.py

--- BoschFutureMobility_final/test/preprocessing_test.py
+++ BoschFutureMobility_final/test/preprocessing_test.py
@@ -58,7 +58,7 @@ while True:
     beta = 0
     alpha_beta_image = cv2.convertScaleAbs(grayscale_roi, alpha=alpha, beta=beta)
     hist_eq = cv2.equalizeHist(alpha_beta_image)
-    bilateral = cv2.bilateralFilter(alpha_beta_image, 9, 5, 15)
+    bilateral = cv2.bilateralFilter(hist_eq, 9, 5, 15)
 
 
 
