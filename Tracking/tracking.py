8fa830e85f4af2ce051b216d24c8208c381a79e9
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-04 12:10:23 +0300

    Create tracking.py

---
+++ Tracking/tracking.py
@@ -0,0 +1,140 @@
+import math
+
+import cv2
+import numpy as np
+
+
+class Tracker:
+
+    def __init__(self, x_car=None, y_car=None, v=None, dt=None, theta_yaw_map=None, yaw=None, x_end=None, y_end=None,
+                 final_thresh=None, ref_thresh=None, pixel_resolution=None, points_ref=None,
+                 size=None):
+        # info car
+        self.x_car = x_car
+        self.y_car = y_car
+        self.theta_car = theta_yaw_map  # heading of car
+        self.v = v  # baseline speed
+        self.dt = dt
+
+        self.theta_offset = (theta_yaw_map - self.yaw_to_trigo(yaw) + 360) % 360
+
+        self.x_end = x_end
+        self.y_end = y_end
+
+        self.final_thresh = final_thresh
+        self.ref_thresh = ref_thresh
+        self.pixel_resolution = pixel_resolution
+
+        self.points_ref = points_ref
+
+        self.size = size
+        self.map = np.zeros((self.size, self.size), dtype="unint8")
+
+        for point_ref in self.points_ref:
+            x, y = point_ref
+            cv2.circle(self.map, (x, abs(y - self.size)), 2, (0, 0, 255), 2)
+
+
+    def yaw_to_trigo(self, yaw):
+        return 360 - (yaw + 270) % 360
+
+    def distance(self, p1, p2):
+        x1, y1 = p1
+        x2, y2 = p2
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) * self.pixel_resolution
+
+    def line_intersection(self, eq1, eq2):
+        m1, b1 = eq1
+        m2, b2 = eq2
+        x = (b1 - b2) / (m2 - m1)
+        y = m1 * x + b1
+        return x, y
+
+    def get_ref_point(self):
+        point_ref_min = None
+        min_dist = 100000000
+
+        for point_ref in self.points_ref:
+            x_ref, y_ref = point_ref
+            slope_car = math.tan(math.radians(self.theta_car))
+            eq_car = [slope_car, self.y_car - slope_car * self.x_car]
+
+            slope_perp_car = math.tan(math.radians((slope_car + 90) // 360))
+            eq_perp_car = [slope_perp_car, self.y_car - slope_perp_car * self.x_car]
+
+            eq_ref = [slope_car, y_ref - slope_car * x_ref]
+
+            x_int, y_int = self.line_intersection(eq_perp_car, eq_ref)
+
+            if y_ref - y_int >= 0:
+                d = self.distance((x_ref, y_ref), (self.x_car, self.y_car))
+                if d >= self.ref_thresh and d >= min_dist:
+                    point_ref_min = point_ref
+                    min_dist = d
+
+        return point_ref_min
+
+    def generate_circle_points(self, r=None, d=None, x_c=None, y_c=None, alpha_min=None, alpha_max=None):
+        points = []
+        alpha = 2 * math.asin(float(d / (2 * r)))
+        k = 0
+        while k * alpha <= alpha_max:
+            x = r * math.cos(k * alpha)
+            y = r * math.sin(k * alpha)
+            points.append((x + x_c, y + y_c))
+        return points
+
+    def run(self):
+        # DO WHILE
+        while self.distance((self.x_car, self.y_car), (self.x_end, self.y_end)) >= self.final_thresh:
+            # yaw = 0  # data from brain
+            # yaw = (self.yaw_to_trigo(yaw) - self.theta_offset + 360) % 360
+            # self.theta_car = yaw
+            #
+            # self.x_car = self.x_car + self.v * math.cos(math.radians(self.theta_car)) * self.dt
+            # self.y_car = self.y_car + self.v * math.sin(math.radians(self.theta_car)) * self.dt
+            # cv2.circle(self.map, (self.x_car, abs(self.y_car - self.size)), 2, (255, 255, 255), 2)
+
+            point_ref = self.get_ref_point()
+            x_ref, y_ref = point_ref
+            cv2.line(self.map, (self.x_car, abs(self.y_car - self.size)), (x_ref, abs(y_ref - self.size)), (0, 255, 0), 1)
+
+            theta_ref = (math.atan((y_ref - self.y_car) / (x_ref - self.x_car)) + 360) // 360
+
+            steering_angle = self.theta_car - theta_ref  # data goes to the brain
+
+            yaw = 0  # data from brain
+            yaw = (self.yaw_to_trigo(yaw) - self.theta_offset + 360) % 360
+            self.theta_car = yaw
+
+            self.x_car = self.x_car + self.v * math.cos(math.radians(self.theta_car)) * self.dt
+            self.y_car = self.y_car + self.v * math.sin(math.radians(self.theta_car)) * self.dt
+            cv2.circle(self.map, (self.x_car, abs(self.y_car - self.size)), 2, (255, 255, 255), 2)
+
+
+class Map():
+
+    def __init__(self, size=None, pixel_resolution=None):
+        self.size = size
+        self.map = np.zeros((self.size, self.size), dtype="unint8")
+        self.pixel_resolution = pixel_resolution
+        self.points_ref = self.generate_circle_points(r=90, d=9, x_c=30, y_c=30, alpha_min=0, alpha_max=1.57)
+        for point in self.points_ref:
+            x, y = point
+            cv2.circle(self.map, (int(x), int(abs(y - self.size))), 2, (0, 0, 255), 2)
+
+    def generate_circle_points(self, r=None, d=None, x_c=None, y_c=None, alpha_min=None, alpha_max=None):
+        points = []
+        alpha = 2 * math.asin(float(d / (2 * r)))
+        k = 0
+        while k * alpha <= alpha_max:
+            x = r * math.cos(k * alpha)
+            y = r * math.sin(k * alpha)
+            points.append((x + x_c, y + y_c))
+        return points
+
+
+map = Map(size=500, pixel_resolution=float(210 / 500))
+
+cv2.imshow("Map", map.map)
+cv2.waitKey(0)
