ee0272f6b6d9a353aa50457269eb274c5cc279f5
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-11 23:52:18 +0300

    Update test.py

--- folder_testing_preprocessing_techniques/test.py
+++ folder_testing_preprocessing_techniques/test.py
@@ -1,3 +1,4 @@
+import time
 import cv2
 
 cap = cv2.VideoCapture(0)
@@ -10,8 +11,11 @@ def preprocess(frame_ROI):
 
 while True:
     _, frame = cap.read()
+    start = time.time()
     frame_ROI = frame[270:, :]
     frame_preprocessed = preprocess(frame_ROI)
     cv2.imshow("ROI Preprocess", frame_preprocessed)
     # cv2.imshow("ROI", frame_ROI)
+    end = time.time()
+    print("time = {}".format(end - start))
     cv2.waitKey(1)
