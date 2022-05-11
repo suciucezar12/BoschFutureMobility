c1daac47b296732bcce2e359f691efe35812bdee
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-11 23:43:52 +0300

    Update test.py

--- folder_testing_preprocessing_techniques/test.py
+++ folder_testing_preprocessing_techniques/test.py
@@ -2,9 +2,12 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+def preprocess(frame_ROI):
+    gray = cv2.cvtColor(frame_ROI, cv2.COLOR_BGR2GRAY)
+    
+
 while True:
     _, frame = cap.read()
     frame_ROI = frame[270:, :]
     cv2.imshow("ROI", frame_ROI)
     cv2.waitKey(1)
-    
