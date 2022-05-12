d2d754fc5bd0a7162ac3d8d3d1a91707501be865
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-12 16:54:25 +0300

    Create get_sample_lane_detection.py

---
+++ get_sample_lane_detection.py
@@ -0,0 +1,27 @@
+import cv2
+import keyboard
+
+cap = cv2.VideoCapture(0)
+
+
+i = 0
+was_pressed = False
+
+while True:
+
+    _, frame = cap.read()
+    if keyboard.is_pressed('a'):
+        if was_pressed == False:
+            i = i + 1
+            print(i)
+            cv2.imwrite("./sample_lane_detection/sample_" + str(i) + ".jpg", frame)
+            print("Sample_" + str(i) + " added!")
+            was_pressed = True
+        else:
+            was_pressed = False
+
+    if keyboard.is_pressed('x'):
+        exit()
+
+    cv2.imshow("Frame", frame)
+    cv2.waitKey(1)
