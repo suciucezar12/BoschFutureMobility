ad09c5cedc830a207c89df461b61506fba450064
suciucezar12 <cezarmihai.suciu@yahoo.ro>
2022-05-12 12:31:54 +0300

    preprocess

--- folder_testing_preprocessing_techniques/test.py
+++ folder_testing_preprocessing_techniques/test.py
@@ -13,6 +13,16 @@ def preprocess(frame_ROI):
     cv2.imshow("canny + hist eq", canny)
     return gaussian
 
+
+def preprocess1(frame_ROI):
+    gray = cv2.cvtColor(frame_ROI, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Gray", gray)
+    # cv2.imshow("canny + gray", cv2.Canny(gray, 100, 250))
+    hist_eq = cv2.equalizeHist(gray)
+    gaussian = cv2.GaussianBlur(hist_eq, ksize=(3,3), sigmaX=0)
+    _, otsu = cv2.threshold(gaussian, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return otsu
+
 while True:
     _, frame = cap.read()
     start = time.time()
---
+++ test_preprocess/hist_eq_gaussian_canny.py
@@ -0,0 +1,29 @@
+import time
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    start = time.time()
+
+    frame_ROI = frame[270:, :]
+
+    gray = cv2.cvtColor(frame_ROI, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("gray", gray)
+
+    hist_eq = cv2.equalizeHist(gray)
+    cv2.imshow("Hist_eq", hist_eq)
+
+    gaussian = cv2.GaussianBlur(hist_eq, ksize=(3, 3), sigmaX=0)
+    # _, otsu = cv2.threshold(gaussian, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    canny = cv2.Canny(hist_eq, 150, 250)
+
+    end = time.time()
+
+    print("time = {}".format(end - start))
+
+    # cv2.imshow("Otsu", otsu)
+    cv2.imshow("Canny", canny)
+    
